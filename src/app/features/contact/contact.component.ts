18d074791784ee4e457c0d3004c7731b988d06e9
Felipe Alencar <gia.comini@hotmail.com>
2025-03-24 16:30:40 -0300

    contact info

--- src/app/features/contact/contact.component.ts
+++ src/app/features/contact/contact.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+import { NzGridModule } from "ng-zorro-antd/grid";
+import { NzLayoutModule } from "ng-zorro-antd/layout";
 
 @Component({
-  selector: 'app-contact',
-  imports: [],
-  templateUrl: './contact.component.html',
-  styleUrl: './contact.component.less'
+  selector: "app-contact",
+  imports: [NzLayoutModule, NzGridModule, CommonModule],
+  templateUrl: "./contact.component.html",
+  styleUrl: "./contact.component.less",
 })
 export class ContactComponent {
+  name: string = "Felipe Alencar";
+  contact: string = "felipe@example.com";
+  country: string = "Brazil";
+  state: string = "São Paulo";
+  phone: string = "+55 11 99999-9999";
+  linkedIn: string = "https://www.linkedin.com/in/felipealencar12/";
+  gitHub: string = "https://github.com/felipe-alencar12";
 
+  contactItems = [
+    { label: "Name", value: this.name },
+    { label: "Contact", value: this.contact, link: "mailto:" + this.contact },
+    { label: "Country", value: this.country + " - " + this.state },
+    { label: "Phone", value: this.phone },
+    { label: "LinkedIn", value: this.linkedIn, link: this.linkedIn },
+    { label: "GitHub", value: this.gitHub, link: this.gitHub },
+  ];
 }
--- src/app/features/host/host.component.ts
+++ src/app/features/host/host.component.ts
@@ -2,10 +2,11 @@ import { Component } from "@angular/core";
 import { NzButtonModule } from "ng-zorro-antd/button";
 import { HeaderComponent } from "../header/header.component";
 import { AboutComponent } from "../about/about.component";
+import { ContactComponent } from "../contact/contact.component";
 
 @Component({
   selector: "app-host",
-  imports: [NzButtonModule, HeaderComponent, AboutComponent],
+  imports: [NzButtonModule, HeaderComponent, AboutComponent, ContactComponent],
   templateUrl: "./host.component.html",
   styleUrl: "./host.component.less",
 })
