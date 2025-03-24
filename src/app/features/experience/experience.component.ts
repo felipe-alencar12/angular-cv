6f91b23ad68d013fef4bfe8200ee1cdf6a2b89c6
Felipe Alencar <gia.comini@hotmail.com>
2025-03-24 14:45:28 -0300

    criando componentes

---
+++ src/app/features/experience/experience.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-experience',
+  imports: [],
+  templateUrl: './experience.component.html',
+  styleUrl: './experience.component.less'
+})
+export class ExperienceComponent {
+
+}
---
+++ src/app/features/host/host.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-host',
+  imports: [],
+  templateUrl: './host.component.html',
+  styleUrl: './host.component.less'
+})
+export class HostComponent {
+
+}
