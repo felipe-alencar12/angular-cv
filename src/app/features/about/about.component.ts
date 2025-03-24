ee78852dcb1c5a8164e47f1782276cc22033cf88
Felipe Alencar <gia.comini@hotmail.com>
2025-03-24 16:17:51 -0300

    component about estilizado

--- src/app/features/about/about.component.ts
+++ src/app/features/about/about.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { NzLayoutModule } from "ng-zorro-antd/layout";
 
 @Component({
-  selector: 'app-about',
-  imports: [],
-  templateUrl: './about.component.html',
-  styleUrl: './about.component.less'
+  selector: "app-about",
+  imports: [NzLayoutModule],
+  templateUrl: "./about.component.html",
+  styleUrl: "./about.component.less",
 })
 export class AboutComponent {
-
+  aboutText: string =
+    "I am a passionate web developer with a focus on creating modern and scalable applications. I enjoy learning new technologies and solving complex problems.";
 }
--- src/app/features/host/host.component.ts
+++ src/app/features/host/host.component.ts
@@ -1,10 +1,11 @@
 import { Component } from "@angular/core";
 import { NzButtonModule } from "ng-zorro-antd/button";
 import { HeaderComponent } from "../header/header.component";
+import { AboutComponent } from "../about/about.component";
 
 @Component({
   selector: "app-host",
-  imports: [NzButtonModule, HeaderComponent],
+  imports: [NzButtonModule, HeaderComponent, AboutComponent],
   templateUrl: "./host.component.html",
   styleUrl: "./host.component.less",
 })
