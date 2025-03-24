6d4e2c1646b0da84b3cc55e2516959d380dba7c2
Felipe Alencar <gia.comini@hotmail.com>
2025-03-24 19:08:11 -0300

    update about component

--- src/app/features/about/about.component.ts
+++ src/app/features/about/about.component.ts
@@ -1,13 +1,19 @@
-import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { Component, Input } from "@angular/core";
 import { NzLayoutModule } from "ng-zorro-antd/layout";
 
 @Component({
   selector: "app-about",
-  imports: [NzLayoutModule],
+  imports: [NzLayoutModule, CommonModule],
   templateUrl: "./about.component.html",
   styleUrl: "./about.component.less",
 })
 export class AboutComponent {
+  @Input() language: string = "en"; // Default to 'en'
+
   aboutText: string =
+    "I am a passionate web developer with a focus on creating modern and scalable applications. I enjoy learning new technologies and solving complex problems.";
+
+  sobreTexto: string =
     "I am a passionate web developer with a focus on creating modern and scalable applications. I enjoy learning new technologies and solving complex problems.";
 }
