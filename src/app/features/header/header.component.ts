feac3eee8030f78f5dd3e28717bae9bc898bc834
Felipe Alencar <gia.comini@hotmail.com>
2025-03-24 15:32:47 -0300

    Criando header

--- src/app/features/header/header.component.ts
+++ src/app/features/header/header.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { NzLayoutModule } from "ng-zorro-antd/layout";
+import { NzMenuModule } from "ng-zorro-antd/menu";
+import { NzIconModule } from "ng-zorro-antd/icon";
 
 @Component({
-  selector: 'app-header',
-  imports: [],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.less'
+  selector: "app-header",
+  imports: [NzLayoutModule, NzMenuModule, NzIconModule],
+  templateUrl: "./header.component.html",
+  styleUrl: "./header.component.less",
 })
 export class HeaderComponent {
-
+  title = "x";
+  subtitle = "y";
 }
--- src/app/features/host/host.component.ts
+++ src/app/features/host/host.component.ts
@@ -1,9 +1,10 @@
 import { Component } from "@angular/core";
 import { NzButtonModule } from "ng-zorro-antd/button";
+import { HeaderComponent } from "../header/header.component";
 
 @Component({
   selector: "app-host",
-  imports: [NzButtonModule],
+  imports: [NzButtonModule, HeaderComponent],
   templateUrl: "./host.component.html",
   styleUrl: "./host.component.less",
 })
