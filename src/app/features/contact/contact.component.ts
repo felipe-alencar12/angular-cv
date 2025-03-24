557912311da338fb2f0575cbbfeb1b0f12800bcd
Felipe Alencar <gia.comini@hotmail.com>
2025-03-24 16:41:41 -0300

    contact info update

--- src/app/features/contact/contact.component.ts
+++ src/app/features/contact/contact.component.ts
@@ -1,29 +1,46 @@
 import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
 import { NzGridModule } from "ng-zorro-antd/grid";
+import { NzIconModule } from "ng-zorro-antd/icon";
 import { NzLayoutModule } from "ng-zorro-antd/layout";
 
 @Component({
   selector: "app-contact",
-  imports: [NzLayoutModule, NzGridModule, CommonModule],
+  imports: [NzLayoutModule, NzGridModule, CommonModule, NzIconModule],
   templateUrl: "./contact.component.html",
   styleUrl: "./contact.component.less",
 })
 export class ContactComponent {
-  name: string = "Felipe Alencar";
-  contact: string = "felipe@example.com";
+  name: string = " Felipe Alencar";
+  contact: string = "gia.comini@hotmail.com";
   country: string = "Brazil";
   state: string = "São Paulo";
-  phone: string = "+55 11 99999-9999";
+  phone: string = "+55 11 95119-2674";
   linkedIn: string = "https://www.linkedin.com/in/felipealencar12/";
   gitHub: string = "https://github.com/felipe-alencar12";
 
   contactItems = [
-    { label: "Name", value: this.name },
-    { label: "Contact", value: this.contact, link: "mailto:" + this.contact },
-    { label: "Country", value: this.country + " - " + this.state },
-    { label: "Phone", value: this.phone },
-    { label: "LinkedIn", value: this.linkedIn, link: this.linkedIn },
-    { label: "GitHub", value: this.gitHub, link: this.gitHub },
+    {
+      value: "Felipe Alencar Giacomini",
+      style: "font-size: 20px; font-weight: bold;color:#0e76a8;",
+    },
+    {
+      value: "gia.comini@hotmail.com",
+      style: "font-size: 16px;",
+    },
+    { value: "Brazil - São Paulo", style: "font-size: 16px;" },
+    { value: "+55 11 95119-2674", style: "font-size: 16px;" },
+    {
+      value: "LinkedIn Profile",
+      link: "https://www.linkedin.com/in/felipealencar12/",
+      icon: "linkedin",
+      style: "color: #0e76a8;",
+    },
+    {
+      value: "GitHub Profile",
+      link: "https://github.com/felipe-alencar12",
+      icon: "github",
+      style: "color: #333;",
+    },
   ];
 }
